bc627d03ed8b36d0165380532a168c9dbbe39f9f
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-05-10 21:21:21 +0800

    Create write-cluster-hosts.demo.sh

---
+++ kube-pods/write-cluster-hosts.demo.sh
@@ -0,0 +1,26 @@
+# kwd=hdfstest && kubectl get po|awk '/'"$kwd"'/{print$1}'|xargs -P0 -i{x} kubectl exec {x} -- /bin/bash -c 'echo "'"$(kubectl get po|awk '/'"$kwd"'/{print$1}'|xargs -P0 -i{x} kubectl exec {x} -- /bin/bash -c 'echo -n "'{x}' " ; hostname -i'|awk 'BEGIN{print}{print$2,$1}')"'" >> /etc/hosts'
+
+kwd=monoabc &&
+kubectl get po |
+    awk '/'"$kwd"'/{print$1}' |
+    xargs -P0 -i{x} kubectl exec {x} -- /bin/bash -c '
+      echo "'"$(
+    
+  kubectl get po |
+      awk '/'"$kwd"'/{print$1}' |
+      xargs -P0 -i{x} kubectl exec {x} -- /bin/bash -c '
+        echo -n "'{x}' " ;
+        hostname -i ;
+      ' |
+      awk 'BEGIN{print}{print$2,$1}' ;
+
+                )"'" >> /etc/hosts ;
+    ' ;
+
+##: 前提: 把 pod 当单节点使用
+##: 这里所有名称中带有 monoabc 的 pod 视为一个集群
+##: 先并发得到所有节点 pod 的 IP 和 hostname
+##: 再并发写入每个节点的 /etc/hosts
+
+##: $()本质是用标准输出拼外层命令的字符串
+##: 所以只必要有一次执行的部分只会执行一次
