f1e6fee02565dc61cba3df08dd1f5417d271e581
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-05-11 10:41:04 +0800

    Update add-jdk-in-clustered-pods.demo.sh

--- kube-pods/add-jdk-in-clustered-pods.demo.sh
+++ kube-pods/add-jdk-in-clustered-pods.demo.sh
@@ -1,1 +1,20 @@
-(未完成)
+# 0
+
+# kubectl get po | awk /clustertest/{print\$1} | xargs -P0 -i{x} kubectl exec {x} -- bash -c 'echo ======== '"'"{x}"'"' ======== >&2 ; ''hostname'
+# kube_alldo () { podskw="${1:-clustertest}"  cmd="${2:-hostname -i}"  parnum="${3:-0}" && kubectl get po | awk /"$podskw"/{print\$1} | xargs -P"$parnum" -i{x} kubectl exec {x} -- bash -c 'echo ======== '"'"{x}"'"' ======== >&2 ; '"$cmd" ; }
+
+kube_alldo ()
+{
+    podskw="${1:-clustertest}" &&
+    cmd="${2:-hostname -i}" &&
+    parnum="${3:-0}" &&
+    kubectl get po |
+        awk /"$podskw"/{print\$1} |
+        xargs -P"$parnum" -i{x} kubectl exec {x} -- bash -c '
+          echo ======== '"'"{x}"'"' ======== >&2 ;
+        '"$cmd" ;
+} ;
+
+# 1
+
+(please wait ...)
