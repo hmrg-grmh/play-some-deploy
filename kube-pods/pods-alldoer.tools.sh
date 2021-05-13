bdd1e68b06b1147aaf621bceceb82f8ebdb4e848
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-05-13 10:22:37 +0800

    Create pods-alldoer.tools.sh

---
+++ kube-pods/pods-alldoer.tools.sh
@@ -0,0 +1,17 @@
+
+pods_pardo ()
+{
+    podskw="${1:-clustertest}" &&
+    cmd="${2:-hostname -i}" &&
+    parnum="${3:-0}" &&
+    kubectl get po |
+        awk /"$podskw"/{print\$1} |
+        xargs -P"$parnum" -i{x} kubectl exec {x} -- bash -c '
+          echo ======== '"'"{x}"'"' ======== >&2 ;
+        '"$cmd" ;
+} &&
+
+pods_alldo () { pods_paringdo "$1" "$2" 0 ; } &&
+pods_perdo () { pods_paringdo "$1" "$2" 1 ; } &&
+
+declare -f $(declare -F | awk /pods_*/)
