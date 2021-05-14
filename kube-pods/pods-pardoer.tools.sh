4fc7a205dd69be2620bfede5cc430560b3b080ff
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-05-14 11:32:19 +0800

    Update pods-pardoer.tools.sh
    add time fmt and time in for time out and in ..

--- kube-pods/pods-pardoer.tools.sh
+++ kube-pods/pods-pardoer.tools.sh
@@ -4,18 +4,20 @@ pods_pardo_x ()
     podskw="${1:-/clustertest/}" &&
     cmd="${2:-hostname -i}" &&
     parnum="${3:-0}" &&
+    timefmt="${4:-%T%::z}" &&
+    
     kubectl get po |
         awk "$podskw"{print\$1} |
         xargs -P"$parnum" -i{x} kubectl exec {x} -- bash -c '
-          echo ======== '"$(date +[%T%::z])"' - '"'"{x}"'"' - "$(date +[%T%::z])" ======== >&2 ;
+          echo ======== '"$(date +["$timefmt"])"' - '"'"{x}"'"' - "$(date +['"$timefmt"'])" ======== >&2 ;
         '"$cmd" ;
 } &&
-pods_alldo_x () { pods_pardo_x "$1" "$2" 0 ; } &&
-pods_perdo_x () { pods_pardo_x "$1" "$2" 1 ; } &&
+pods_alldo_x () { pods_pardo_x "$1" "$2" 0 "$3" ; } &&
+pods_perdo_x () { pods_pardo_x "$1" "$2" 1 "$3" ; } &&
 
-pods_pardo () { pods_pardo_x /"$1"/ "$2" "$3" ; } &&
-pods_alldo () { pods_pardo "$1" "$2" 0 ; } &&
-pods_perdo () { pods_pardo "$1" "$2" 1 ; } &&
+pods_pardo () { pods_pardo_x /"$1"/ "$2" "$3" "$4" ; } &&
+pods_alldo () { pods_pardo "$1" "$2" 0 "$3" ; } &&
+pods_perdo () { pods_pardo "$1" "$2" 1 "$3" ; } &&
 
 
 # end show message
