a85e07b80b1f6e1f50ffdca78dfad6febb7a22b8
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-05-14 21:27:24 +0800

    chgs after tired ....
    去掉了一些冗余和以前会用的。
    
    就用完整写 awk 代码的版本罢，糖 (all|per) 不要了。
    更改了 ns 的参数位置，同时增加了 sh 解释器的选择。
    
    PS: 我不想用 getopt(s) 。

--- kube-pods/pods-pardoer.tools.sh
+++ kube-pods/pods-pardoer.tools.sh
@@ -1,57 +1,40 @@
 
 pods_pardo_x ()
 {
-    namespacename="${1:-default}" &&
-    case $# in
-        5)  shift ;;
-        4)  ;;
-        *)  echo plz use these two: &&
-            echo \ pods_pardo_x '<'namespacename'|'"''"'>' '<'podsawkcode'|'"''"'>' '<'cmd'|'"''"'>' '<'parnum'|'"''"'>' '<'timefmt'|'"''"'>' \; &&
-            echo \ pods_pardo_x  '<'podsawkcode'|'"''"'>' '<'cmd'|'"''"'>' '<'parnum'|'"''"'>' '<'timefmt'|'"''"'>' \; &&
-            return 45 ;;
-    esac &&
     podsawkcode="${1:-/clustertest/}" &&
     cmd="${2:-hostname -i}" &&
     parnum="${3:-0}" &&
     timefmt="${4:-%T%::z}" &&
     
-    kubectl get po -o custom-columns=:.metadata.name --namespace "$namespacename" |
-        awk "$podsawkcode"{print\$1} |
-        xargs -P"$parnum" -i{x} kubectl exec {x} -- bash -c '
+    namespace_opt="${5:---namespace default}" &&
+    shparser="${6:-bash}" &&
+    
+    kubectl get po -o custom-columns=:.metadata.name   $namespace_opt |
+        awk "$podsawkcode" |
+        xargs -P"$parnum" -i{x} kubectl exec {x} -- "$shparser" -c '
           echo ======== '"$(date +["$timefmt"])"' - '"'"{x}"'"' - "$(date +['"$timefmt"'])" ======== >&2 ;
         '"$cmd" ;
 } &&
 
-
-
 pods_def_alldo_x () { pods_pardo_x "$1" "$2" 0 "$3" ; } &&
 pods_def_perdo_x () { pods_pardo_x "$1" "$2" 1 "$3" ; } &&
-
-pods_ns_alldo_x () { pods_pardo_x "$1" "$2" "$3" 0 "$4" ; } &&
-pods_ns_perdo_x () { pods_pardo_x "$1" "$2" "$3" 1 "$4" ; } &&
-
-pods_alldo_x () { pods_pardo_x "$1" "$2" "$3" 0 "$4" ; } &&
-pods_perdo_x () { pods_pardo_x "$1" "$2" "$3" 1 "$4" ; } &&
-
-
+podsns_alldo_x () { pods_pardo_x "$1" "$2" "$3" 0 "$4" ; } &&
+podsns_perdo_x () { pods_pardo_x "$1" "$2" "$3" 1 "$4" ; } &&
 
 pods_def_pardo () { pods_pardo_x /"$1"/ "$2" "$3" "$4" ; } &&
-pods_def_alldo () { pods_pardo "$1" "$2" 0 "$3" ; } &&
-pods_def_perdo () { pods_pardo "$1" "$2" 1 "$3" ; } &&
+pods_def_alldo () { pods_def_pardo "$1" "$2" 0 "$3" ; } &&
+pods_def_perdo () { pods_def_pardo "$1" "$2" 1 "$3" ; } &&
+podsns_pardo () { pods_pardo_x "$1" /"$2"/ "$3" "$4" "$5" ; } &&
+podsns_alldo () { podsns_pardo "$1" "$2" "$3" 0 "$4" ; } &&
+podsns_perdo () { podsns_pardo "$1" "$2" "$3" 1 "$4" ; } &&
 
-pods_ns_pardo () { pods_pardo_x "$1" /"$2"/ "$3" "$4" "$5" ; } &&
-pods_ns_alldo () { pods_pardo "$1" "$2" "$3" 0 "$4" ; } &&
-pods_ns_perdo () { pods_pardo "$1" "$2" "$3" 1 "$4" ; } &&
-
-pods_alldo () { pods_pardo "$1" "$2" "$3" 0 "$4" ; } &&
-pods_perdo () { pods_pardo "$1" "$2" "$3" 1 "$4" ; } &&
 
 
 
 
 # end show message
-declare -f $(declare -F | awk /pods_*/\&\&\!/_x/\&\&\!/_def_/\&\&\!/_ns_/) ;
+declare -f $(declare -F | awk /pods_*/\&\&\!/_def_/\&\&\!/ns_/) ;
 # or
-declare -F | awk /pods_*/\&\&\!/_x/\&\&\!/_def_/\&\&\!/_ns_/{print\$0'" ;"'} | xargs echo [MSG]: to see some defined pods-funcs: >&2 ;
+declare -F | awk /pods_*/\&\&\!/_def_/\&\&\!/ns_/{print\$0'" ;"'} | xargs echo [MSG]: to see some defined pods-funcs: >&2 ;
 
 
